dd6a7f47ed8b7007c6d1a7eea1f0535e2a2b7e1f
Gert-Ranno Lumine <gert-ranno.kask@khk.ee>
2021-12-15 08:45:45 +0200

    Get weather data from API

--- classes/Weather.js
+++ classes/Weather.js
@@ -3,9 +3,10 @@ class Weather {
         this.city = city
         this.key = '1be874f1963f5abf75004820b5bfd946'
     }
-   async getWeather() {
+
+    async getWeather() {
         const responce = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.key}`)
-       const responceData = await responce.json()
-       return responceData
+        const responceData = await responce.json()
+        return responceData
     }
 }
